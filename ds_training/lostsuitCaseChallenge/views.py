ab3a9c36e00b02c6752a42f552d7ea2b403f4cb3
ricoregala <rico.regala@hotmail.cm>
2020-03-13 14:37:23 +0800

    Renamed Folders

--- ds_training/lostsuitCaseChallenge/views.py
+++ ds_training/lostsuitCaseChallenge/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
+def test0(request):
     return HttpResponse('TEEEEEEST!!!')
+
+def test1(request):
+    return HttpResponse('123eresdkfhskgjsgsf!!!')
